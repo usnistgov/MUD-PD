05757e374c2d5e0557a677febe54a1ea088f3dfa
Paul Watrobski <ptw@nist.gov>
2019-03-27 19:03:10 -0400

    Major redesign and reformat of GUI. Still in the very early stages of development. Changes made to the database class as well.

--- pyDBscripts/MudPcapAnalyticsGUI.py
+++ pyDBscripts/MudPcapAnalyticsGUI.py
@@ -1,6 +1,272 @@
+#!/usr/bin/python3
+
+from database import CaptureDatabase
+import sys
+
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+'''
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+'''
+#from tkinter import messagebox
+
+dbFields = 'host', 'database', 'user', 'passwd'
+captureFields = 'File', 'Activity', 'Details'
+deviceFields = 'Model', 'Internal Name', 'Device Category', 'Communication Standards', 'Notes'
+deviceOptions = 'WiFi', 'Bluetooth', 'Zigbee', 'ZWave', '4G', '5G', 'Other'
+deviceStateFields = 'Firmware Version' #maybe include this with device fields entry and note that it will be associated with the capture only
+
+fields = 'Last Name', 'First Name', 'Job', 'Country'
+
+#GUI Class for the MUD Capture Analysis
+class  MUDcapGUI(tk.Frame):
+    def __init__(self,parent=None): 
+        tk.Frame.__init__(self,parent)
+        self.parent = parent
+        self.pack()
+        #self.makeDbForm()
+        self.ents = self.make_form_database(dbFields)
+
+        #self.bind('<Return>', (lambda event, e=ents: self.fetch(e)))   
+        self.bind('<Return>', (lambda event, e=self.ents: self.database_connect(e)))   
+
+        self.b_connect = tk.Button(self.parent, text='Connect',
+                         #command=(lambda e=ents: self.fetch(e)))
+                         command=(lambda e=self.ents: self.database_connect(e)))
+        self.b_quit = tk.Button(self.parent, text='Quit', command=self.__exit__)#self.parent.quit)
+
+        if sys.platform == "win32":
+            self.b_quit.pack(side=tk.RIGHT, padx=5, pady=5)
+            self.b_connect.pack(side=tk.RIGHT, padx=5, pady=5)
+        else:
+            self.b_connect.pack(side=tk.RIGHT, padx=5, pady=5)
+            self.b_quit.pack(side=tk.RIGHT, padx=5, pady=5)
+
+    def make_form_database(self, fields):
+        self.winfo_toplevel().title("MUD Capture - Connect to Database")
+
+        entries = []
+
+        try:
+            db_config = read_db_config()
+        except:
+            db_config = {"host": "", "database" : "", "user" : "", "passwd" : ""}
+
+        for field in fields:
+            row = tk.Frame(self)
+            #row = Frame(root)
+            lab = tk.Label(row, width=15, text=field, anchor='w')
+            if field == "passwd":
+                ent = tk.Entry(row, show="\u2022", width=15)
+            else:
+                ent = tk.Entry(row)
+            ent.insert( 10, db_config.get(field,"none") )
+
+            row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=2)
+            lab.pack(side=tk.LEFT)
+            ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+            entries.append((field, ent))
+
+        return entries
+
+    def fetch(entries):
+        for entry in entries:
+            field = entry[0]
+            text  = entry[1].get()
+            print('%s: "%s"' % (field, text)) 
+
+    def database_connect(self, entries):
+        db_config = {}
+
+        for entry in entries:
+            field = entry[0]
+            text  = entry[1].get()
+            db_config[field] = text
+            print('%s: "%s"' % (field, text)) 
+
+        self.db = CaptureDatabase(db_config)
+
+    def __exit__(self):
+        try:
+            self.db.__exit__()
+            print("Cleaned up on exit")
+        except:
+            print("Problem with cleanup")
+
+        self.parent.quit()
+
+
+
+
+
+
+def openFileCallBack(entry):
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+
+    #filename = tk.filedialog.askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    entry.delete(0, END)
+    entry.insert(0, filename)
+
+def makeCaptureForm(root, fields):
+    entries = []
+    for i, field in enumerate(fields):
+        row = Frame(root)
+        lab = Label(row, width=15, text=field, anchor='w')
+        ent = Entry(row)
+        
+        if i == 0:
+            b = Button(row, text='...', command=(lambda e=ent: openFileCallBack(e)))#openFileCallBack())
+            row.pack(side=TOP, fill=X, padx=5, pady=5)
+            lab.pack(side=LEFT)
+            ent.pack(side=LEFT, fill=X)
+            b.pack(side=LEFT, expand=YES, fill=X)
+        else:
+            row.pack(side=TOP, fill=X, padx=5, pady=5)
+            lab.pack(side=LEFT)
+            ent.pack(side=RIGHT, expand=YES, fill=X)
+            
+        entries.append((field, ent))
+
+    return entries
+         
+def fetch(entries):
+    for entry in entries:
+        field = entry[0]
+        text  = entry[1].get()
+        print('%s: "%s"' % (field, text)) 
+
+def makeform(root, fields):
+    entries = []
+    for field in fields:
+        row = Frame(root)
+        lab = Label(row, width=15, text=field, anchor='w')
+        ent = Entry(row)
+        row.pack(side=TOP, fill=X, padx=5, pady=5)
+        lab.pack(side=LEFT)
+        ent.pack(side=RIGHT, expand=YES, fill=X)
+        entries.append((field, ent))
+
+    return entries
+    
+def importFileWindow(entry):
+    #Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    importFile = Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    
+    f_details = makeImportForm(importFile, captureFields)
+    importFile.bind('<Return>', (lambda event, e=ents: fetch(e)))
+
+    b_submit = Button(importFile, text="Submit",
+                      command=(lambda e=ents: fetch(e)))
+    b_submit.pack(side=LEFT, padx=5, pady=5)
+
+    b_submit = Button(importFile, text="Quit", command=root.quit)
+    b_submit.pack(side=LEFT, padx=5, pady=5)
+
+    importFile.mainloop()
+
+#class 
+
+def makeDbForm(root, fields):
+    entries = []
+
+    try:
+        db_config = read_db_config()
+    except:
+        db_config = {"host": "", "database" : "", "user" : "", "passwd" : ""}
+    
+    for field in fields:
+        row = Frame(root)
+        lab = Label(row, width=15, text=field, anchor='w')
+        if field == "passwd":
+            ent = Entry(row, show="\u2022", width=15)
+        else:
+            ent = Entry(row)
+            #ent.insert( 10, db_config.get(field,"none") )
+        ent.insert( 10, db_config.get(field,"none") )
+
+        #print(field + " = " + db_config.get(field,"none"))
+
+        row.pack(side=TOP, fill=X, padx=5, pady=2)
+        lab.pack(side=LEFT)
+        ent.pack(side=RIGHT, expand=YES, fill=X)
+        entries.append((field, ent))
+
+    return entries
+
+
+#Password field:
+#widget = Entry(parent, show="\u2022", width=15) #show="*"
+from configparser import ConfigParser
+def read_db_config(filename='config.ini', section='mysql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+
+    gui = MUDcapGUI(root)
+
+    '''
+    #ents = makeform(root, fields)
+    ents = makeDbForm(root, dbFields)
+    #dbents = makeDbForm(root, dbFields)
+    #ents = makeCaptureForm(root, captureFields)
+    root.bind('<Return>', (lambda event, e=ents: fetch(e)))   
+
+    b1 = Button(root, text='Submit',
+                command=(lambda e=ents: fetch(e)))
+    b2 = Button(root, text='Quit', command=root.quit)
+
+    if sys.platform == "win32":
+        b2.pack(side=RIGHT, padx=5, pady=5)
+        b1.pack(side=RIGHT, padx=5, pady=5)
+    else:
+        b1.pack(side=RIGHT, padx=5, pady=5)
+        b2.pack(side=RIGHT, padx=5, pady=5)
+
+
+
+
+
+
+    '''
+    '''
+    if sys.platform == "win32":
+        b1 = Button(root, text='Submit',
+                    command=(lambda e=ents: fetch(e)))
+        b1.pack(side=LEFT, padx=5, pady=5)
+
+        b2 = Button(root, text='Quit', command=root.quit)
+        b2.pack(side=LEFT, padx=5, pady=5)
+    else:
+        b2 = Button(root, text='Quit', command=root.quit)
+        b2.pack(side=LEFT, padx=5, pady=5)
+
+        b1 = Button(root, text='Submit',
+                    command=(lambda e=ents: fetch(e)))
+        b1.pack(side=LEFT, padx=5, pady=5)
+    '''
+    root.mainloop()
+
+
+'''
 #! /usr/bin/python
 
 import tkinter
+from tkinter.filedialog import askopenfilename
 from tkinter import messagebox
 
 top = tkinter.Tk()
@@ -9,7 +275,17 @@ top.title("MUD Packet Capture GUI")
 def helloCallBack():
     messagebox.showinfo( "Hello Python", "Hello World")
 
-B = tkinter.Button(top, text ="Hello", command = helloCallBack)
-B.pack()
+def openFileCallBack():
+    tkinter.Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    print(filename)
+    
+
+B_hello = tkinter.Button(top, text ="Hello", command = helloCallBack)
+B_hello.pack()
+
+B_file = tkinter.Button(top, text ="File", command = openFileCallBack)
+B_file.pack()
 
 top.mainloop()
+'''
--- pyDBscripts/database.py
+++ pyDBscripts/database.py
@@ -1,9 +1,98 @@
 #! /usr/bin/python3
 
 import hashlib
+
+#MySQl libraries
 import mysql.connector
+from mysql.connector import MySQLConnection, Error
+from configparser import ConfigParser
+
 import os
 import pyshark
+
+
+class CaptureDatabase:
+
+
+    '''    
+    def __init__(self, *initial_data, **kwargs):
+        for dictionary in initial_data:
+            for key in dictionary:
+                setattr(self, key, dictionary[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+    '''
+    def __init__(self, db_config):
+        try:
+            print("Connecting to MySQL database...")
+            self.conn = mysql.connector.connect(**db_config)
+
+            if self.conn.is_connected():
+                print("connection established.")
+            else:
+                print("connection failed.")
+
+        except Error as error:
+            print(error)
+        #finally:
+        #    self.conn.close()
+        #    print("Connection closed.")
+
+    def __exit__(self):
+        self.conn.close()
+        print("Connection closed.")
+
+    #def connect(self, 
+'''
+def connect():
+    # Connect to MySQL database
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="MudDB123!",
+            database="DeviceCaptures")
+
+        if conn.is_connected():
+            print("Connected to MySQL database")
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+'''
+
+def read_db_config(filename='config.ini', section='mysql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+def connect():
+    # Connect to MySQL database
+    db_config = read_db_config()
+
+    try:
+        print("Connecting to MySQL database...")
+        conn = mysql.connector.connect(**db_config)
+
+        if conn.is_connected():
+            print("connection established.")
+        else:
+            print("connection failed.")
+
+    except Error as error:
+        print(error)
+    finally:
+        conn.close()
+        print("Connection closed.")
 
 '''
 mydb = mysql.connector.connect(
@@ -90,10 +179,13 @@ class CaptureDigest:
                 #print(pIP)
                 self.uniqueIP_dst.append(pIP_dst)
 
-    def __del__(self):
+    #def __del__(self):
+    def __exit__(self):
         self.cap.close()
 
 
+# Tools for collecting information ***
+# MAC Address Lookup
 import json
 import requests
 def lookup_mac(mac):
@@ -108,7 +200,7 @@ def lookup_mac(mac):
         print(company)
         return company
 
-
+# Hostname Lookup
 import socket
 def lookup_hostname(ip_addr):
     try:
@@ -125,7 +217,12 @@ def lookup_hostname(ip_addr):
         print(r[0])
         return r[0]
 
+
+# Database Main (for testing purposes)
 if __name__== "__main__":
+
+    connect()
+
     fname = "/Users/ptw/Documents/GRA-MITRE-DDoS/captures/ecobee/ecobeeThermostat_iphone_setup.pcap"
     capture = CaptureDigest(fname)
     #import_file(fname)
@@ -141,19 +238,65 @@ if __name__== "__main__":
     for mac in capture.uniqueMAC:
         lookup_mac(mac)
         print(mac + "\r\n")
+        break
 
     print("Unique DST IP addresses:")
     for ip in capture.uniqueIP_dst:
         lookup_hostname(ip)
         print(ip + "\n")
+        break
 
     print("\n\nUnique DST IPv6 addresses:")
     for ipv6 in capture.uniqueIPv6_dst:
         lookup_hostname(ipv6)
         print(ipv6 + "\n")
+        break
 
     mac = "BC:92:6B:A0:00:01"
     company = lookup_mac(mac)
     
     ip_addr = "216.220.61.236"
     lookup_hostname(ip_addr)
+
+
+#Adding capture things items:
+    #fileName
+    #fileLoc - manually input to generate filename, md5, and capDate
+    #md5
+    #capDate
+    #Activity - manual
+    #Details - manual
+
+#Adding device items:
+    #Mfr - attempt to generate from MAC
+    #Model - manual
+    #MAC_addr - Can be located in MAC address
+    #internalName - manual
+    #Device category - manual
+    #MUD capable - may be able to generate this, but manual for now
+    #wifi #if MAC found, then wifi is set to YES
+    #bluetooth - manual
+    #zigbee - manual
+    #zwave - manual
+    #4G - manual
+    #5G - manual
+    #other protocols - manual
+    #notes - manual
+
+#Adding Device State items:
+    #md5 - generated from input fileLoc
+    #MAC address - identified from file
+    #internal name (previously given)
+    #fw_ver - manual
+    #ipv4_addr - generated from file
+    #ipv6_addr - generated from file
+
+#Adding Protocol items:
+    #md5 - generated from input fileLoc
+    #MAC address - generated from file
+    #src_port - generated from input file
+    #dst_ip_addr - generated from file
+    #ipv6 (bool) - generated from file
+    #dst_url - generated from file
+    #dst_port - generated from file
+    #notes - generated

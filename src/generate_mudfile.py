ed1dced7fbaf51a155f1896539a4113cdd4e6ed0
Paul T Watrobski <paul.watrobski@gmail.com>
2019-10-17 13:50:07 -0400

    Method written for merging PCAPs and generating a configuration file to feed to MUDGEE to generate a MUD file. Currently in development: Written, but needs to be tested, debugged, and integrated.

---
+++ src/generate_mudfile.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import pyshark
+import os
+import tempfile
+
+'''
+def merge_captures(capture_files):
+    path = os.getcwd() + '/tmp'
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        print("Creation of the directory %s failed" % path)
+        return
+'''
+
+def gen_mudfile(config, capture_files):
+    
+    with tempfile.TemporaryDirectory() as dir:
+        # Merge capture files
+        merge_command = 'mergecap -w ' + dir '/merged_caps.pcap' + ' %s'*len(capture_files) % capture_files
+        os.system(merge_command)
+
+        # Create configuration file for MUDGEE
+        config['pcapLocation'] = os.getcwd() + '/' + dir + '/merged_caps.pcap'
+        with open(dir + '/temp_config.json', 'w') as fp:
+            json.dump(config, fp)
+
+        # Generate MUD File with MUDGEE
+        config_command = 'java -jar ../mudgee/target/mudgee-1.0.0-SNAPSHOT.jar ' + dir + '/temp_config.json'
+        os.system(config_command)
+
+        # Move MUD File from MUDGEE
+        mv_command = 'mv ../mudgee/results/* mudfiles/'
+        
+        

02d19175123c2a7889ad22d7a6d67c733c969092
Paul T Watrobski <paul.watrobski@gmail.com>
2021-03-19 19:22:12 -0400

    Cleaning up MUDgee mudfile generation before removal due to deprecation

--- src/generate_mudfile.py
+++ src/generate_mudfile.py
@@ -76,39 +76,7 @@ class MUDgeeWrapper:
 
             # Move MUD File from MUDGEE
             print("Moving MUD file from MUDgee to mudpi/mudpd")
+            if not os.path.exists("./mudfiles"):
+                os.mkdir("./mudfiles")
             mv_command = 'mv result/ mudfiles/'
             os.system(mv_command)
-
-
-'''
-def merge_captures(capture_files):
-    path = os.getcwd() + '/tmp'
-
-    try:
-        os.mkdir(path)
-    except OSError:
-        print("Creation of the directory %s failed" % path)
-        return
-'''
-
-'''
-def gen_mudfile(config, capture_files):
-    
-    with tempfile.TemporaryDirectory() as dir:
-        # Merge capture files
-        merge_command = 'mergecap -w ' + dir + '/merged_caps.pcap' + ' %s'*len(capture_files) % capture_files
-        print('merge_command', merge_command)
-        os.system(merge_command)
-
-        # Create configuration file for MUDGEE
-        config['pcapLocation'] = os.getcwd() + '/' + dir + '/merged_caps.pcap'
-        with open(dir + '/temp_config.json', 'w') as fp:
-            json.dump(config, fp)
-
-        # Generate MUD File with MUDGEE
-        config_command = 'java -jar ../mudgee/target/mudgee-1.0.0-SNAPSHOT.jar ' + dir + '/temp_config.json'
-        os.system(config_command)
-
-        # Move MUD File from MUDGEE
-        mv_command = 'mv ../mudgee/results/* mudfiles/'
-'''
